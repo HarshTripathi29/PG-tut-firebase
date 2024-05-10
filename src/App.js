1ad0578b8f675e09783a3e46f9ddf004d81dbaa7
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-05-10 19:10:34 +0530

    onAuthStateChange, logout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
+import { getAuth, onAuthStateChanged, signOut } from 'firebase/auth';
 import './App.css';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import { useEffect, useState } from 'react';
+import { app } from './utils/firebase';
+
 
 function App() {
 
+  const auth = getAuth(app);
+  const [user, setUser] = useState(null);
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user)=>{
+      if(user){
+        setUser(user)
+      }
+      else{
+        console.log("you are logged out");
+        setUser(user);
+      }
+    });
+  },[]);
+
+  if(user==null){
+    return(
+      <div>
+        <SignUp/>
+    <SignIn/>
+      </div>
+    )
+  }
+
+
   return (
     <div className="App">
-    <h1>Firebase react app</h1>
-    <SignUp/>
-    <SignIn/>
+    <h1>Hello {user.email}</h1>
+    <button onClick={()=>signOut(auth)}>Logout</button>
     </div>
   );
 }
